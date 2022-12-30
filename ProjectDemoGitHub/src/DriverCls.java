347293cef5886ca733d290412b68caf69b067eda
Rohitchinnu10 <77531553+Rohitchinnu10@users.noreply.github.com>
2022-12-30 14:20:23 +0530

    Update DriverCls.java

--- ProjectDemoGitHub/src/DriverCls.java
+++ ProjectDemoGitHub/src/DriverCls.java
@@ -4,7 +4,12 @@ public class DriverCls {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Hello...Github");
+		Scanner s;
+		s=new Scanner(System.in);
+		String x;
+		x=s.Next();
+		
+		System.out.println(""+x);
 		
 	}
 
